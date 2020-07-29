2c223d6390e70b033a928a971d03e4a3682c2a55
Danish Shakeel <danishshakeel54@gmail.com>
2020-07-30 01:57:01 +0530

    Heroku Compatibility

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -126,5 +126,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"staticfiles")
 MEDIA_ROOT =  os.path.join(BASE_DIR,"media")
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
